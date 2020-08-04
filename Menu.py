1a365bb52e922f5963d4499ed06bd82d26633314
tijanap27 <68991760+tijanap27@users.noreply.github.com>
2020-08-04 19:15:15 +0100

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -2,15 +2,32 @@ import tkinter as tk
 
 master = tk.Tk()
 
+# declare the window
+window = tk.Tk()
+# set window title
+window.title("Notes")
+# set window width and height
+window.configure(width=500, height=300)
+# set window background color
+window.configure(bg='lightblue')
+window.mainloop()
+
 
 #New_Note = tk.Button(master, text="New Note", command)
 Job = tk.Button (master, text="Job", command = open("Job_input.txt","r"), width=20);
+Job.grid(row=0, column=0)
 School = tk.Button (master, text="School", command = open("School_input.txt","r"), width=20);
+School.grid(row=1, column=0)
 FaF = tk.Button (master, text="Family and Friends", command = open("FaF_input.txt","r"), width=20);
+FaF.grid(row=2, column=0)
 House = tk.Button (master, text="House", command = open("House_input.txt","r"), width=20);
+House.grid(row=3, column=0)
 Travel = tk.Button (master, text="Travel", command = open("Travel_input.txt","r"), width=20);
+Travel.grid(row=4, column=0)
 Extracurricular = tk.Button (master, text="Extracirricular", command = open("Extracurricular_input.txt","r"), width=20);
+Extracurricular.grid(row=5, column=0)
 Passwords = tk.Button (master, text="Passwords", command = open("Passwords_input.txt","r"), width=20);
+Passwords.grid(row=5, column=0)
 
 Job.pack()
 School.pack()
