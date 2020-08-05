462af3b02bb736583744b03e0ebbf5adfdb4cdba
Dra9ana <69038885+Dra9ana@users.noreply.github.com>
2020-08-05 13:47:09 +0200

    Add files via upload

--- Job.py
+++ Job.py
@@ -11,51 +11,47 @@ class Job:
 
 def read_input():
     #gets begin and end from input boxes ad store them in begin_time, end_time, daysoff
-    begin_time   = input1.get()
-    end_time     = input2.get()
-    days_off = input3.get()
-     
+    name         = input1.get()
+    begin_time   = input2.get()
+    end_time     = input3.get()
+    days_off     = input4.get()
+
      #writes data in file input.txt
     input_file = open("Job_input.txt","w");
-    input_file. write(begin_time+" "+end_time+" "+days_off);
+    input_file. write(name+" "+begin_time+","+end_time+","+days_off);
     input_file.close()
-     
-     #creates window?
-    new_master = tk.Toplevel(master)
+
+def output_section(new_master, list):
+
     #label=what is written on the window, and sets position
-    tk.Label(new_master, text="Beginning time:"+begin_time).grid(row=0)
-    tk.Label(new_master, text="Ending time:"+end_time).grid(row=1)
-    tk.Label(new_master, text="Number of days off left:"+days_off).grid(row=2)
-    #not sure, mislim da obeybedjuje da ne nestane vec ostane vidljiv dok ga ne izgasis
-    new_master.mainloop()
+    tk.Label(new_master, text="Beginning time:"+list[0]).grid(row=0)
+    tk.Label(new_master, text="Ending time:"+list[1]).grid(row=1)
+    tk.Label(new_master, text="Number of days off left:"+[list[2]]).grid(row=2)
 
+def new_note(master):
+    #making three labels for description of Job section
+    tk.Label(master, text="Note name:").grid(row=0)
+    tk.Label(master, text="Beginning time:").grid(row=1)
+    tk.Label(master, text="Ending time:").grid(row=2)
+    tk.Label(master, text="Number of days-off left:").grid(row=3)
 
+    # making input boxes
+    input1 = tk.Entry(master)
+    input2 = tk.Entry(master)
+    input3 = tk.Entry(master)
+    input4 = tk.Entry(master)
 
+    #setting position
+    input1.grid(row=0, column=1)
+    input2.grid(row=1, column=1)
+    input3.grid(row=2, column=1)
+    input4.grid(row=3, column=1)
 
+    #making buttons and setting actions, command=what will happen when you click the button,sticky, pady not sure
+    tk.Button(master, text='Quit', command=master.quit).grid(row=4, column=1, sticky=tk.W,pady=4)
+    tk.Button(master, text='Done', command=read_input ).grid(row=4, column=2, sticky=tk.W, pady=4)
 
 #creates master
 master = tk.Tk()
-
-
-#making three labels for description of Job section
-tk.Label(master, text="Beginning time:").grid(row=0)
-tk.Label(master, text="Ending time:").grid(row=1)
-tk.Label(master, text="Number of days-off left:").grid(row=2)
-
-# making input boxes
-input1 = tk.Entry(master)
-input2 = tk.Entry(master)
-input3 = tk.Entry(master)
-
-#setting position
-input1.grid(row=0, column=1)
-input2.grid(row=1, column=1)
-input3.grid(row=2, column=1)
-
-#making buttons and setting actions, command=what will happen when you click the button,sticky, pady not sure 
-tk.Button(master, text='Quit', command=master.quit).grid(row=3, column=1, sticky=tk.W,pady=4)
-tk.Button(master, text='Done', command=read_input).grid(row=3, column=2, sticky=tk.W, pady=4)
-
-
-
+new_note(master)
 master.mainloop()
