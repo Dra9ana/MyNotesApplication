a33f9a5e97ef86a8829e74dfa20cb4f5822a2974
tijanap27 <68991760+tijanap27@users.noreply.github.com>
2020-08-05 12:07:58 +0100

    Update New_Job.py

--- New_Job.py
+++ New_Job.py
@@ -62,8 +62,8 @@ input4.grid(row=4, column=1)
 input5.grid(row=5, column=1)
 
 #making buttons and setting actions, command=what will happen when you click the button,sticky, pady not sure 
-tk.Button(master, text='Quit', command=master.quit).grid(row=6, column=1, sticky=tk.W,pady=4)
-tk.Button(master, text='Done', command=read_input).grid(row=6, column=2, sticky=tk.W, pady=4)
+tk.Button(master, text='Quit', command=master.quit).grid(row=7, column=1, sticky=tk.W,pady=4)
+tk.Button(master, text='Done', command=read_input).grid(row=7, column=2, sticky=tk.W, pady=4)
 
 
 
