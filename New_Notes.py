2b9e96b40fd86c737d028e90d643b3e716eaa7b4
tijanap27 <68991760+tijanap27@users.noreply.github.com>
2020-08-07 12:38:17 +0100

    Update New_Notes.py

--- New_Notes.py
+++ New_Notes.py
@@ -1,29 +1,29 @@
 import sqlite3
 
-def New_Notes(s): 
+def New_Notes(s, a): 
   conn = sqlite3.connect('Notes.db')
   
   if(s=='Job'):
     conn.execute("INSERT INTO Job (Topic, Date, Time, Duration, Description) VALUES(?, ?, ?, ?, ?)", 
-                                                  (topic, date, time, duration, description))     
+                                                  (a[0], a[1], a[2], a[3], a[4]))     
     elif(s=='FaF'):
         conn.execute("INSERT INTO FaF (Name, Activity, Date, Time, Duration, Description) VALUES(?, ?, ?, ?, ?, ?)", 
-                                                  (name, activity, date, time, duration, description)) 
+                                                  (a[0], a[1], a[2], a[3], a[4], a[5])) 
     elif(s=='School'):
         conn.execute("INSERT INTO School (Topic, Subject, Activity, Date, Time, Duration, Description) VALUES(?, ?, ?, ?, ?, ?, ?)", 
-                                                 (topic, subject, activity, date, time, duration, description)) 
+                                                 (a[0], a[1], a[2], a[3], a[4], a[5], a[6])) 
     elif(s=='Travel'):
         conn.execute("INSERT INTO Travel (Name, Destination, Date, Time, Duration, Description) VALUES(?, ?, ?, ?, ?, ?)", 
-                                                  (name, destination, date, time, duration, description)) 
+                                                  (a[0], a[1], a[2], a[3], a[4], a[5])) 
     elif(s=='Extracurricular'):
         conn.execute("INSERT INTO Extracurricular (Name, Activity, Date, Time, Description) VALUES(?, ?, ?, ?, ?)", 
-                                                  (name, activity, date, time, description)) 
+                                                  (a[0], a[1], a[2], a[3], a[4])) 
     elif(s=='Passwords'):
         conn.execute("INSERT INTO Passwords (Website, Additional_info, Username, Mail, Password) VALUES(?, ?, ?, ?, ?)", 
-                                                  (website, additional_info, username, mail, password)) 
+                                                  (a[0], a[1], a[2], a[3], a[4])) 
     elif(s=='House'):
         conn.execute("INSERT INTO House (Name, Activity, Date, Time, Description) VALUES(?, ?, ?, ?, ?)", 
-                                                  (name, activity, date, time, description)) 
+                                                  (a[0], a[1], a[2], a[3], a[4])) 
 
 
 conn.commit() 
