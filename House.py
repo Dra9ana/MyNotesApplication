46ac955513131b72e5d691d0719deb828b17446b
tijanap27 <68991760+tijanap27@users.noreply.github.com>
2020-09-05 15:12:33 +0100

    Update House.py

--- House.py
+++ House.py
@@ -1,4 +1,4 @@
-# House section
+# HOUSE SECTION
 from tkinter import *
 import tkinter as tk
 import New_Notes
@@ -32,7 +32,7 @@ def output_section(master, list_box, indexes_list):
         # Set column index and its relative weight to distributeadditional space between
         master.grid_columnconfigure(2, weight=1)
 
-        # Labelling Important Points
+        # Label Important Points
         tk.Label(new_master, text="Name:"+s[1], width=20, height=2).grid(row=1, column=2)
         tk.Label(new_master, text="Activity:"+s[2], width=20, height=2).grid(row=2, column=2)
         tk.Label(new_master, text="Date:"+s[3], width=20, height=2).grid(row=3, column=2)
@@ -40,10 +40,10 @@ def output_section(master, list_box, indexes_list):
         tk.Label(new_master, text="Description:"+s[5], width=20, height=2).grid(row=5, column=2)
 
 def new_note (list_box, indexes_list):
-    # Creating master
+    # Create master
     master = tk.Tk()
 
-    # Making three labels for description of School section
+    # Make three labels for description of School section
     tk.Label(master, text="House notes:").grid(row=0)
     tk.Label(master, text="Name:").grid(row=1)
     tk.Label(master, text="Activity:").grid(row=2)
@@ -51,20 +51,20 @@ def new_note (list_box, indexes_list):
     tk.Label(master, text="Time:").grid(row=4)
     tk.Label(master, text="Description:").grid(row=5)
 
-    # Making input boxes
+    # Make input boxes
     input1 = tk.Entry(master)
     input2 = tk.Entry(master)
     input3 = tk.Entry(master)
     input4 = tk.Entry(master)
     input5 = tk.Entry(master)
 
-    # Setting position of a box
+    # Set position of a box
     input1.grid(row=1, column=1)
     input2.grid(row=2, column=1)
     input3.grid(row=3, column=1)
     input4.grid(row=4, column=1)
     input5.grid(row=5, column=1)
 
-    # Making buttons and setting actions
+    # Make buttons and setting actions
     tk.Button(master, text='Quit', command=master.quit).grid(row=7, column=1, sticky=tk.W,pady=4)
     tk.Button(master, text='Done', command=lambda:read_input(master, input1, input2, input3, input4, input5, list_box, indexes_list)).grid(row=7, column=2, sticky=tk.W, pady=4)
