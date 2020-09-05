2f1310570f92a7c0d09f6c3a433289c47a272625
tijanap27 <68991760+tijanap27@users.noreply.github.com>
2020-09-05 20:30:25 +0100

    Update Extracurricular.py

--- Extracurricular.py
+++ Extracurricular.py
@@ -74,6 +74,6 @@ def new_note (list_box, indexes_list):
        input5.grid(row=5, column=1)
        input6.grid(row=6, column=1)
 
-       # Make buttons and setting actions
-       tk.Button(master, text='Quit', command=master.quit).grid(row=8, column=1, sticky=tk.W,pady=4)
-       tk.Button(master, text='Done', command=lambda:read_input(master, input1, input2, input3, input4, input5, input6, list_box, indexes_list)).grid(row=8, column=2, sticky=tk.W, pady=4)
+     # Make buttons and setting actions
+    tk.Button(master, text='Quit', command=master.quit).grid(row=7, column=2, sticky=tk.W, pady=4)
+    tk.Button(master, text='Done', command=lambda:read_input(master, input1, input2, input3, input4, input5, list_box, indexes_list)).grid(row=7, column=3, sticky=tk.W, pady=4)
