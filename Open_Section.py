18ce043fbeb39e9b6bba870a37d9991a299f765c
tijanap27 <68991760+tijanap27@users.noreply.github.com>
2020-09-05 16:23:25 +0100

    Update Open_Section.py

--- Open_Section.py
+++ Open_Section.py
@@ -1,3 +1,5 @@
+# CREATES A WINDOW FOR A CHOSEN SECTION 
+
 from tkinter import *
 import tkinter as tk
 from  New_Notes import*
@@ -34,12 +36,12 @@ def open_section(master, section):
      list_box.select_set(first = 0, last = None)
      list_box.event_generate("<<ListboxSelect>>")
 
-     #make buttons
+     # Make buttons
      deln = tk.Button(master, text="Delete", command=lambda:Delete_Note(section, list_box, indexes_list), width='8',bg='white', fg='black')
      addn = tk.Button(master, text="New Note",command=lambda:Add_Note(section, list_box, indexes_list),width='8', bg='white', fg='black')
      edit = tk.Button(master, text="Edit",command=lambda:Edit_Note(section, master),width='8', bg='white', fg='black')
 
-     #set places for buttons
+     # Set places for buttons
      deln.place(x = 800.0,y = 20.0)
      addn.place(x = 800.0,y = 40.0)
      edit.place(x = 800.0,y = 60.0)
