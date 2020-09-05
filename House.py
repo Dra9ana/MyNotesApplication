03711382f9216ce51617eef8f8634eb08ba10a9a
tijanap27 <68991760+tijanap27@users.noreply.github.com>
2020-09-05 20:31:14 +0100

    Update House.py

--- House.py
+++ House.py
@@ -67,5 +67,5 @@ def new_note (list_box, indexes_list):
     input5.grid(row=5, column=1)
 
     # Make buttons and setting actions
-    tk.Button(master, text='Quit', command=master.quit).grid(row=7, column=1, sticky=tk.W,pady=4)
-    tk.Button(master, text='Done', command=lambda:read_input(master, input1, input2, input3, input4, input5, list_box, indexes_list)).grid(row=7, column=2, sticky=tk.W, pady=4)
+    tk.Button(master, text='Quit', command=master.quit).grid(row=7, column=2, sticky=tk.W,pady=4)
+    tk.Button(master, text='Done', command=lambda:read_input(master, input1, input2, input3, input4, input5, list_box, indexes_list)).grid(row=7, column=3, sticky=tk.W, pady=4)
