bc582f546724c58a4f1a9e84e5cdf6bec287572c
tijanap27 <68991760+tijanap27@users.noreply.github.com>
2020-09-13 11:51:01 +0100

    Update Database.py

--- Database.py
+++ Database.py
@@ -2,8 +2,7 @@
 
 import sqlite3
 
-global id_allowance 
-global id_balance 
+global id_total
 
 # Create database
 conn = sqlite3.connect('Notes.db')
@@ -46,19 +45,11 @@ c.execute('''CREATE TABLE Allowance
 c.execute('''CREATE TABLE Expences
              ([generated_id] INTEGER PRIMARY KEY, [Date] date, [Incomes] float, [Food] float, [Clothes] float, [Bills] float, [Direct_debts] float, [Other] float, [Total] float)''')
 
-# Create table - Balance
-c.execute('''CREATE TABLE Balance
-             ([generated_id] INTEGER PRIMARY KEY, [Incomes] float, [Food] float, [Food_left] float, [Clothes] float, [Clothes_left] float, [Bills] float, [Bills_left] float, [Direct_debts] float, [Direct_debts_left] float, [Other] float, [Other_left] float, [Total] float, [Left] float)''')
-
-c.execute("INSERT INTO Allowance (Food, Clothes, Bills, Direct_debts, Other, Total) VALUES(?, ?, ?, ?, ?, ?)",
-                               (0, 0, 0, 0, 0, 0))
-id_allowance=c.lastrowid
-c.execute("INSERT INTO Balance (Incomes, Food, Food_left, Clothes, Clothes_left, Bills, Bills_left, Direct_debts, Direct_debts_left, Other, Other_left, Total, Left) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                               (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
-id_balance=c.lastrowid
-
-def expences_ids():
-    return [id_allowance, id_balance]
+cursor=conn.cursor()
+cursor.execute("INSERT INTO Expences (Date, Incomes, Food, Clothes, Bills, Direct_debts, Other, Total) VALUES(?, ?, ?, ?, ?, ?, ?, ?)",
+                                                  (0/0/0, 0, 0, 0, 0, 0, 0, 0))
+# Get id of last row inserted into database
+id_total=cursor.lastrowid
 
 # Commit changes
 conn.commit()
